5e9ec10d41f1c0967e403e1bd4be3f4163ebbca9
MrDLC7 <mafia78ton@gmail.com>
2024-09-19 22:57:14 +0300

    #Start

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace RegEditApplication
 {
@@ -10,6 +7,31 @@ namespace RegEditApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введіть розширення файлу (наприклад, .txt)");
+
+            string fileExtension = Console.ReadLine();
+
+            string registryPath = @"HKEY_CLASSES_ROOT\" + fileExtension;
+
+            try
+            {
+                //Спроба отримати значення реєстру
+                object regValue = Registry.GetValue(registryPath, "", null);
+
+                if (regValue != null)
+                {
+                    Console.WriteLine($"Значення для розширення {fileExtension}: {regValue}");
+                }
+                else
+                {
+                    Console.WriteLine($"Розділ для розширення {fileExtension} не знайдено.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Виникла помилка в доступі до реєстру: {ex.Message}");
+            }
+            Console.ReadKey();
         }
     }
 }
